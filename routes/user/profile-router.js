0a628db94e48a80b4080ef54517cd89f9fb8decf
Bessie Arino <brarino@gmail.com>
2018-03-12 10:58:51 -0700

    trying to finish profile router, not sure if doing image correctly

--- routes/user/profile-router.js
+++ routes/user/profile-router.js
@@ -3,48 +3,91 @@
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const debug = require('debug')('sportsapp:profile-router');
-// const createError = require('http-errors');
+const createError = require('http-errors');
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const Profile = require('../../model/user/profile.js');
 const bearerAuth = require('../../lib/bearer-auth-middleware.js');
 
 const profileRouter = module.exports = Router();
 
-profileRouter.post('/api/profile', bearerAuth, jsonParser, function(req, res, next) {
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const s3 = new AWS.S3();
+const dataDir = `${__dirname}/../data`;
+const upload = multer({ dest: dataDir });
+
+function s3uploadProm(params) {
+  debug('s3uploadProm');
+
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, s3data) => {
+      // if(err) reject(err);
+      // console.log('s3data: ', s3data);
+      resolve(s3data);
+    });
+  });
+}
+
+profileRouter.post('/api/profile', bearerAuth, upload.single('image'), jsonParser, function (req, res, next) {
   debug('POST: /api/profile');
 
   req.body.userID = req.user._id;
   new Profile(req.body).save()
-    .then( profile => res.json(profile))
+    .then(profile => res.json(profile))
     .catch(next);
+
+  let ext = path.extname(req.file.originalname);
+
+  let params = {
+    ACL: 'public-read',
+    Bucket: process.env.AWS_BUCKET,
+    Key: `${req.file.filename}${ext}`,
+    Body: fs.createReadStream(req.file.path),
+  };
 });
 
-profileRouter.get('/api/profile/:profileId', bearerAuth, function(req, res, next) {
+profileRouter.get('/api/profile/:profileId', bearerAuth, function (req, res, next) {
   debug('GET: /api/profile/:profileId');
 
   Profile.findById(req.params.profileId)
-    .then( profile => res.json(profile))
+    .then(profile => res.json(profile))
     .catch(next);
 });
 
-profileRouter.put('/api/profile/:profileId', bearerAuth, jsonParser, function(req, res, next) {
+profileRouter.put('/api/profile/:profileId', bearerAuth, jsonParser, function (req, res, next) {
   debug('PUT: /api/profile:profileId');
 
-  // TO DO: CREATE 400 ERROR IF NO REQ BODY
-  // TO DO: CREATE 400 ERROR IF NO PROFILE ID
+  if (!req.body) {
+    return next(createError(400, 'request body not provided'));
+  }
 
-  Profile.findByIdAndUpdate(req.params.profileId, req.body, {new: true})
-    .then( profile => res.json(profile))
+  Profile.findByIdAndUpdate(req.params.profileId, req.body, { new: true })
+    .then(profile => res.json(profile))
     .catch(next);
 });
 
+// PUT route without a profileId
+profileRouter.put('/api/profile', bearerAuth, jsonParser, function (req, res, next) {
+  debug('PUT: /api/profile');
 
-profileRouter.delete('/api/profile/:profileId', bearerAuth, function(req, res, next) {
+  return next(createError(400, 'profileId not provided'));
+});
+
+profileRouter.delete('/api/profile/:profileId', bearerAuth, function (req, res, next) {
   debug('DELETE: /api/profile/:profileId');
 
-  // TO DO: CREATE 400 ERROR IF NO PROFILE ID
-
   Profile.findByIdAndRemove(req.params.profileId)
-    .then( () => res.status(204).send())
+    .then(() => res.status(204).send())
     .catch(next);
+});
+
+// DELETE route without a profileId
+profileRouter.delete('api/profile', bearerAuth, function (req, res, next) {
+  debug('DELETE: /api/profile');
+
+  return next(createError(400, 'profileId not provided'));
 });
