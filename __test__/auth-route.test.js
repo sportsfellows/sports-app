1cc6ec8157655ac6298b206fe4aef6915c61c590
Brian Bixby <brianbixby0@gmail.com>
2018-03-11 21:03:30 -0700

    all over the place

--- __test__/auth-route.test.js
+++ __test__/auth-route.test.js
@@ -1,1 +1,140 @@
 'use strict';
+
+const request = require('superagent');
+const User = require('../model/user/user.js');
+const serverToggle = require('../lib/server-toggle.js');
+const server = require('../server.js');
+
+require('jest');
+
+const url = 'http://localhost:3000';
+
+const exampleUser = {
+  username: 'exampleuser',
+  password: '1234',
+  email: 'exampleuser@test.com',
+};
+
+describe('Auth routes', function() {
+  beforeAll( done => {
+    serverToggle.serverOn(server, done);
+  });
+  afterAll( done => {
+    serverToggle.serverOff(server, done);
+  });
+
+  describe('POST: /api/signup', function() {
+    describe('with a valid body', function() {
+      afterEach( done => {
+        User.remove({})
+          .then( () => done())
+          .catch(done);
+      });
+
+      it('should return a token', done => { 
+        request.post(`${url}/api/signup`)
+          .send(exampleUser)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
+      });
+
+      it('should return a 400 error, no body', done => {
+        request.post(`${url}/api/signup`)
+          .send()
+          .set({
+            Authorization: `Bearer ${this.tempToken}`,
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(400);
+            done();
+          });
+      });
+  
+    });
+  });
+
+  describe('GET: /api/signin', function() {
+    describe('with a valid body', function() {
+      beforeEach( done => {
+        let user = new User(exampleUser);
+        user.generatePasswordHash(exampleUser.password)
+          .then( user => user.save())
+          .then( user => {
+            this.tempUser = user;
+            done();
+          })
+          .catch(done);
+      });
+      afterEach( done => {
+        User.remove({})
+          .then( () => done())
+          .catch(done);
+      });
+
+      it('return a token', done => {
+        request.get(`${url}/api/signin`)
+          .auth('exampleuser', '1234')
+          .end((err, res) => {
+            if(err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
+      });
+
+      it('should return a 401 when user cant be authenticated', done => {
+        request.get(`${url}/api/signin`)
+          .auth('')
+          .end((err, res) => {
+            expect(res.status).toEqual(401);
+            done();
+          });
+      });
+    });
+  });
+});
+
+// describe('PUT: /api/signin', function() {
+//   describe('with a valid body', function() {
+//     beforeEach( done => {
+//       new User(exampleUser)
+//         .generatePasswordHash(exampleUser.password)
+//         .then( user => {
+//           this.tempUser = user;
+//           return user.generateToken();
+//         })
+//         .then( token => {
+//           this.tempToken = token;
+//           done();
+//         })
+//         .catch(done);
+//     });
+//     afterEach( done => {
+//       User.remove({})
+//         .then( () => done())
+//         .catch(done);
+//     });
+
+//     it('should update and return a list with a 200 status', done => {
+//       request.put(`${url}/api/signin`)
+//         .send(updatedUser)
+//         .set({
+//           Authorization: `Bearer ${this.tempToken}`,
+//         })
+//         .end((err, res) => {
+//           if (err) return done(err);
+//           console.log(res);
+//           expect(res.status).toEqual(200);
+//           expect(res.body.username).toEqual(updatedUser.username);
+//           expect(res.body.email).toEqual(updatedUser.email);
+//           expect(res.body.userID).toEqual(this.tempUser.id.toString());
+//           done();
+//         });
+//     });
+
+//   });
+// });
--- routes/sportingEvent/sportingEvent-router.js
+++ routes/sportingEvent/sportingEvent-router.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('sportsapp:profile-router');
+const createError = require('http-errors');
+
+const SportingEvent = require('../../model/sportingEvent/sportingEvent.js');
+const bearerAuth = require('../../lib/bearer-auth-middleware.js');
+
+const sportingEventRouter = module.exports = Router();
+
+sportingEventRouter.post('/api/sportingevent', bearerAuth, jsonParser, function(req, res, next) {
+  debug('POST: /api/sportingEvent');
+
+  if (!req.body.sportingEventName || !req.body.desc ) return next(createError(400, 'expected a request body name and desc'));
+  new SportingEvent(req.body).save()
+    .then( sportingEvent => res.json(sportingEvent))
+    .catch(next);
+});
+
+sportingEventRouter.get('/api/sportingevent/:sportingEventId', bearerAuth, function(req, res, next) {
+  debug('GET: /api/sportingEvent/:sportingEventId');
+
+  SportingEvent.findById(req.params.sportingEventId)
+    .then( sportingEvent => res.json(sportingEvent))
+    .catch(next);
+});
+
+sportingEventRouter.put('/api/sportingevent/:sportingEventId', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT: /api/sportingEvent:sportingEventId');
+
+  // TO DO: CREATE 400 ERROR IF NO REQ BODY
+  // TO DO: CREATE 400 ERROR IF NO SPORTING EVENT ID
+
+  SportingEvent.findByIdAndUpdate(req.params.sportingEventId, req.body, {new: true})
+    .then( sportingEvent => res.json(sportingEvent))
+    .catch(next);
+});
+
+
+sportingEventRouter.delete('/api/sportingevent/:sportingEventId', bearerAuth, function(req, res, next) {
+  debug('DELETE: /api/sportingEvent/:sportingEventId');
+
+  // TO DO: CREATE 400 ERROR IF NO SPORTING EVENT ID
+
+  SportingEvent.findByIdAndRemove(req.params.sportingEventId)
+    .then( () => res.status(204).send())
+    .catch(next);
+});
