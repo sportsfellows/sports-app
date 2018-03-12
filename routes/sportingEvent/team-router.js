e1a985e6a8f61521c2d4baa43363c08c060b7da6
kennieU <ken.unterseher@gmail.com>
2018-03-12 11:20:59 -0700

    ready for team-router.js errors

--- routes/sportingEvent/team-router.js
+++ routes/sportingEvent/team-router.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('sportsapp:team-router');
+const createError = require('http-errors');
+
+const Team = require('../model/sportingEvent/team.js');
+const bearerAuth = require('../..lib/bearer-auth-middleware.js');
+
+const teamRouter = module.exports = Router();
+
+teamRouter.post('/api/team', bearerAuth, jsonParser, function(req, res, next) {
+  debug('POST: /api/team');
+
+  if (!req.body.team || req.body.desc)
+    return next(createError(400, 'expected a request body name and desc'));
+  new Team(req.body).save()
+    .then( team => res.json(team))
+    .catch(next);
+});
+
+teamRouter.get('/api/team/:teamId', bearerAuth, function(req, res, next) {
+  debug('GET: /api/team/:teamId');
+
+  Team.findById(req.params.team)
+    .then( team => res.json(team))
+    .catch(next);
+});
+
+teamRouter.put('/api/team/:teamId', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT: /api/team:teamId');
+
+  // TO DO: CREATE 400 ERROR IF NO REQ BODY
+  // TO DO: CREATE 400 ERROR IF NO TEAM ID
+
+  Team.findByIdAndUpdate(req.params.teamId, req.body, {new:true})
+    .then( team => res.json(team))
+    .catch(next);
+});
+
+teamRouter.delete('api/team/:teamId');
+
+// TO DO: CREATE 400 ERROR IF NO TEAM ID
+
+Team.findByIdAndRemove(req.params.teamId)
+  .then( () => res.status(204).send())
+  .catch(next);
