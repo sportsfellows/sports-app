ee2760706288f86ccc25bbce76ec5160f74d351c
kennieU <ken.unterseher@gmail.com>
2018-03-14 12:59:50 -0700

    working on team test

--- __test__/team-route.test.js
+++ __test__/team-route.test.js
@@ -2,6 +2,7 @@
 
 const request = require('superagent');
 const Team = require('../model/sportingEvent/team.js');
+const SportingEvent = require('../model/sportingEvent/sportingEvent.js');
 const serverToggle = require('../lib/server-toggle.js');
 const server = require('../server.js');
 
@@ -10,18 +11,46 @@ require('jest');
 
 const url = 'http://localhost:3000';
 
-const exampleTeam = {
-  teamName: 'example team name',
-  content: 'example content',
-};
+const { exampleTeam, exampleSportingEvent } = require('./lib/lib/mockData.js');
 
 describe('Team Router', function() {
   beforeAll( done => {
     serverToggle.serverOn(server, done);
   });
-  afterAll( done => {
+
+  beforeAll(done => {
+    new Team(exampleTeam)
+      .then(team => team.save())
+      .then(team => {
+        this.tempTeam = team;
+        exampleTeam.ID = this.tempTeam._id;
+        return team();
+      })
+      .catch(done);
+  });
+
+  afterAll(done => {
     serverToggle.serverOff(server, done);
   });
+
+  afterEach(done => {
+    Promise.all([
+      Team.remove({}),
+    ])
+      .then(() => done())
+      .catch(done);
+  });
+
+  //   describe('GET: /api/team/:teamId', () => {
+  //     it('should give a 200 resonse code with a valid id', done => {
+  //       request.get(`${url}/api/team/${this.tempTeam._id}`)
+  //         .end((err, res) => {
+  //           expect(res.status).toEqual(200);
+  //           done();
+  //         });
+  //     });
+  //   });
+  // });
 
   describe('GET: /api/team/:teamId', function() {
     describe('with a valid id', function() {
@@ -46,6 +75,10 @@ describe('Team Router', function() {
               })
               .catch( err => done(err));
           }
+        });
+
+        afterAll(done => {
+          serverToggle.serverOff(server, done);
         });
 
         afterAll( done => {
