b24e1c515822b76274a46faaaf6a47a360eeba3b
kennieU <ken.unterseher@gmail.com>
2018-03-14 15:53:05 -0700

    working on team tests

--- __test__/team-route.test.js
+++ __test__/team-route.test.js
@@ -11,94 +11,105 @@ require('jest');
 
 const url = 'http://localhost:3000';
 
-const { exampleTeam, exampleSportingEvent } = require('./lib/lib/mockData.js');
+const { exampleTeam, exampleSportingEvent } = require('./lib/mockData.js');
 
-describe('Team Router', function() {
-  beforeAll( done => {
+describe('SportingEvent routes', function () {
+  beforeAll(done => {
     serverToggle.serverOn(server, done);
   });
-
-  beforeAll(done => {
-    new Team(exampleTeam)
-      .then(team => team.save())
-      .then(team => {
-        this.tempTeam = team;
-        exampleTeam.ID = this.tempTeam._id;
-        return team();
-      })
-      .catch(done);
-  });
-
   afterAll(done => {
     serverToggle.serverOff(server, done);
   });
 
-  afterEach(done => {
-    Promise.all([
-      Team.remove({}),
-    ])
-      .then(() => done())
-      .catch(done);
-  });
-
-  //   describe('GET: /api/team/:teamId', () => {
-  //     it('should give a 200 resonse code with a valid id', done => {
-  //       request.get(`${url}/api/team/${this.tempTeam._id}`)
-  //         .end((err, res) => {
-  //           expect(res.status).toEqual(200);
-  //           done();
-  //         });
-  //     });
-  //   });
-  // });
-
-  describe('GET: /api/team/:teamId', function() {
-    describe('with a valid id', function() {
-      beforeEach( done => {
-        Team.createTeam(exampleTeam)
-          .then( team => {
-            this.tempTeam = team;
-            done();
-          })
-          .catch( err => done(err));
-      });
+  describe('GET: /api/sportingEvent/:sportingEventId && /api/sportingEvent', () => {
+    beforeEach(done => {
+      new Team(exampleTeam)
+        .then(team => {
+          this.tempTeam = team;
+          return team();
+        })
+        // .then(token => {
+        //   this.tempToken = token;
+        //   done();
+        // })
+        .catch(done);
     });
 
-    describe('DELETE: /api/team/:teamId', () => {
-      describe('with teamId deleted', () => {
-        beforeEach( done => {
-          if (this.createTeam) {
-            Team.createTeam(exampleTeam)
-              .then( team => {
-                this.tempTeam = team;
-                done();
-              })
-              .catch( err => done(err));
-          }
-        });
-
-        afterAll(done => {
-          serverToggle.serverOff(server, done);
-        });
-
-        afterAll( done => {
-          Team.deleteTeam(this.tempTeam.id)
-            .then( () => done())
-            .catch( err => done(err));
-        });
-
-        it('should return teamId deleted', done => {
-          request.delete(`${url}/api/team/${this.tempTeam.id}`)
-            .end((err, res) => {
-              if (err) return done(err);
-              expect(res.status).toEqual(204);
-              expect(res.body.id).toEqual(null);
-              expect(res.body.name).toEqual(null);
-              expect(res.body.content).toEqual(null);
-              done();
-            });
-        });
-      });
+    beforeEach(done => {
+      exampleSportingEvent.teamID = this.tempTeam.id.toString();
+      new SportingEvent(exampleSportingEvent).save()
+        .then(sportingEvent => {
+          this.tempSportingEvent = sportingEvent;
+          done();
+        })
+        .catch(done);
     });
   });
+});
+
+afterEach(() => {
+  delete exampleSportingEvent.teamID;
+});
+
+it('should return a sportingEvent and a 200 status', done => {
+  request.get(`${url}/api/sportingEvent/${this.tempSportingEvent.id}`)
+    .set({
+      Authorization: `Bearer ${this.tempToken}`,
+    })
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.status).toEqual(200);
+      expect(res.body.name).toEqual(exampleSportingEvent.name);
+      expect(res.body.desc).toEqual(exampleSportingEvent.desc);
+      expect(res.body.teamID).toEqual(this.tempTeam.id.toString());
+      done();
+    });
+});
+
+it('should return a 401 when no token is provided', done => {
+  request.get(`${url}/api/sportingEvent/${this.tempSportingEvent.id}`)
+    .set({
+      Authorization: 'Bearer',
+    })
+    .end((err, res) => {
+      expect(res.status).toEqual(401);
+      done();
+    });
+});
+
+it('should return a 404 for a valid req with a sportingEvent id not found', done => {
+  request.get(`${url}/api/sportingEvent/a979e472c577c679758e018`)
+    .set({
+      Authorization: `Bearer ${this.tempToken}`,
+    })
+    .end((err, res) => {
+      expect(res.status).toEqual(404);
+      done();
+    });
+});
+
+it('should return all sportingEvents and a 200 status', done => {
+  request.get(`${url}/api/sportingEvents`)
+    .set({
+      Authorization: `Bearer ${this.tempToken}`,
+    })
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.status).toEqual(200);
+      expect(res.body.name).toEqual(exampleSportingEvent.name);
+      expect(res.body.desc).toEqual(exampleSportingEvent.desc);
+      expect(res.body.teamID).toEqual(this.tempTeam.id.toString());
+      done();
+    });
+});
+
+it('should return a 401 when no token is provided', done => {
+  request.get(`${url}/api/sportingEvents`)
+    .set({
+      Authorization: 'Bearer',
+    })
+    .end((err, res) => {
+      expect(res.status).toEqual(401);
+      done();
+    });
 });
