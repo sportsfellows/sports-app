e721eade58aa1853bbdb011f592d7b9745166813
Brian Bixby <brianbixby0@gmail.com>
2018-03-15 12:10:32 -0700

    profile has 95%cov and auth has 100% cov

--- __test__/profile-router.test.js
+++ __test__/profile-router.test.js
@@ -9,6 +9,8 @@ require('jest');
 
 const url = 'http://localhost:3000';
 
+const updatedProfile = { username: 'updatedUserName'};
+
 describe('Profile routes', function() {
   beforeAll( done => {
     serverToggle.serverOn(server, done);
@@ -54,17 +56,83 @@ describe('Profile routes', function() {
             done();
           });
       });
-    //     it('should return a 400 error, no body', done => {
-    //       request.post(`${url}/api/signup`)
-    //         .send()
-    //         .set({
-    //           Authorization: `Bearer ${this.tempToken}`,
-    //         })
-    //         .end((err, res) => {
-    //           expect(res.status).toEqual(400);
-    //           done();
-    //         });
-    //     });
+
+      it('should return a 401 when no token is provided', done => {
+        request.get(`${url}/api/profile/${this.mock.profile._id}`)
+          .set({
+            Authorization: 'Bearer',
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(401);
+            done();
+          });
+      });
+  
+      it('should return a 404 for a valid req with a list id not found', done => {
+        request.get(`${url}/api/profile/a979e472c577c679758e018`)
+          .set({
+            Authorization: `Bearer ${this.mock.token}`,
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(404);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('PUT: /api/profile/:profileId', () => {
+    describe('with a valid body', () => {
+      it('should update and return a list with a 200 status', done => {
+        request.put(`${url}/api/profile/${this.mock.profile._id}`)
+          .send(updatedProfile)
+          .set({
+            Authorization: `Bearer ${this.mock.token}`,
+          })
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(res.body.username).toEqual(updatedProfile.username);
+            expect(res.body.userID.toString()).toEqual(this.mock.profile.userID.toString());
+            done();
+          });
+      });
+
+      it('should  not update and return a 400 status for invalid req', done => {
+        request.put(`${url}/api/profile/${this.mock.profile._id}`)
+          .send()
+          .set({
+            Authorization: `Bearer ${this.mock.token}`,
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(400);
+            done();
+          });
+      });
+
+      it('should  not update and return a 401 status', done => {
+        request.put(`${url}/api/profile/${this.mock.profile._id}`)
+          .send(updatedProfile)
+          .set({
+            Authorization: `Bearer `,
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(401);
+            done();
+          });
+      });
+
+      it('should  not update and return a 404 status for user list not found', done => {
+        request.put(`${url}/api/profile/a979e472c577c679758e018`)
+          .send(updatedProfile)
+          .set({
+            Authorization: `Bearer ${this.mock.token}`,
+          })
+          .end((err, res) => {
+            expect(res.status).toEqual(404);
+            done();
+          });
+      });
     });
   });
 });
