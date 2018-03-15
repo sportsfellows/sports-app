c85f3f669e6fdca077a2581f7060f848d52e521a
kennieU <ken.unterseher@gmail.com>
2018-03-15 16:11:36 -0700

    team test running, fake team made

--- __test__/lib/fakeTeam.js
+++ __test__/lib/fakeTeam.js
@@ -2,27 +2,42 @@
 
 const faker = require('faker');
 const Team = require('../../model/sportingEvent/team.js');
+const sportingEventMockFactory = require('./fakeSportingEvent.js');
+const fakeUser = require('./fakeUser.js');
 const teamMockFactory = module.exports = {};
 
+// creates a mock team with a team name, seed, record
 teamMockFactory.create = () => {
   let mock = {};
-  mock.request = {
-    teamName: faker.internet.firstName(),
+  mock.teamRequest = {
+    teamName: faker.name.firstName(),
+    seed: faker.random.number(),
+    pretournamentRecord: faker.random.number() + ' - ' + faker.random.number(),
   };
 
-  let team = new Team(mock.request);
-  return team.generatePasswordHash(mock.request.password)
-    .then(team => team.save())
+  // creates a sporting event with an id
+  return sportingEventMockFactory.create()
+    .then(sportingEvent => {
+      mock.sportingEvent = sportingEvent;
+      mock.teamRequest.sportingEventID = sportingEvent._id;
+
+      return new Team(mock.teamRequest).save();
+    })
     .then(team => {
       mock.team = team;
-      return team;
+
+      return fakeUser.create();
     })
-    .then(team => team.generateToken())
-    .then(token => {
-      mock.token = token;
+    .then(mockObject => {
+      mock.token = mockObject.token;
+      mock.user = mockObject.user;
+
       return mock;
-    })
-    .catch(console.log);
+    });
 };
 
-teamMockFactory.remove = () => Team.remove({});
+teamMockFactory.remove = () => Promise.all([
+  Team.remove({}),
+  sportingEventMockFactory.remove({}),
+  fakeUser.remove(),
+]);
--- __test__/team-route.test.js
+++ __test__/team-route.test.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const request = require('superagent');
-const Team = require('../model/sportingEvent/team.js');
-const SportingEvent = require('../model/sportingEvent/sportingEvent.js');
 const serverToggle = require('../lib/server-toggle.js');
 const server = require('../server.js');
-const fakeUser = require('./lib/fakeUser.js');
+// const fakeSportingEvent = require('./lib/fakeSportingEvent.js');
+const fakeTeam = require('./lib/fakeTeam.js');
+// const fakeUser = require('./lib/fakeUser.js');
 
 require('jest');
 
 const url = 'http://localhost:3000';
-
-const { exampleTeam, exampleSportingEvent } = require('./lib/mockData.js');
 
 describe('SportingEvent routes', function () {
   beforeAll(done => {
@@ -21,95 +19,17 @@ describe('SportingEvent routes', function () {
     serverToggle.serverOff(server, done);
   });
 
-  describe('GET: /api/sportingEvent/:sportingEventId', () => {
-    beforeEach(done => {
-      new Team(exampleTeam)
-        .then(team => {
-          this.tempTeam = team;
-          return team();
-        })
-        .then(token => {
-          this.tempToken = token;
-          done();
-        })
-        .catch(done);
-    });
+  beforeEach( () => {
+    return fakeTeam.create()
+      .then(mock => {
+        this.mock = mock;
+      });
+  });
 
-    beforeEach(done => {
-      exampleSportingEvent.teamID = this.tempTeam.id.toString();
-      new SportingEvent(exampleSportingEvent).save()
-        .then(sportingEvent => {
-          this.tempSportingEvent = sportingEvent;
-          done();
-        })
-        .catch(done);
-    });
+  afterEach(fakeTeam.remove);
+
+  it('should', () => {
+    console.log(JSON.stringify(this.mock, null, 2));
   });
 });
-
-afterEach(() => {
-  delete exampleSportingEvent.teamID;
-});
-
-it('should return a sportingEvent and a 200 status', done => {
-  request.get(`${url}/api/sportingEvent/${this.tempSportingEvent.id}`)
-    .set({
-      Authorization: `Bearer ${this.tempToken}`,
-    })
-    .end((err, res) => {
-      if (err) return done(err);
-      expect(res.status).toEqual(200);
-      expect(res.body.name).toEqual(exampleSportingEvent.name);
-      expect(res.body.desc).toEqual(exampleSportingEvent.desc);
-      expect(res.body.teamID).toEqual(this.tempTeam.id.toString());
-      done();
-    });
-});
-
-it('should return a 401 when no token is provided', done => {
-  request.get(`${url}/api/sportingEvent/${this.tempSportingEvent.id}`)
-    .set({
-      Authorization: 'Bearer',
-    })
-    .end((err, res) => {
-      expect(res.status).toEqual(401);
-      done();
-    });
-});
-
-it('should return a 404 for a valid req with a sportingEvent id not found', done => {
-  request.get(`${url}/api/sportingEvent/a979e472c577c679758e018`)
-    .set({
-      Authorization: `Bearer ${this.tempToken}`,
-    })
-    .end((err, res) => {
-      expect(res.status).toEqual(404);
-      done();
-    });
-});
-
-it('should return all sportingEvents and a 200 status', done => {
-  request.get(`${url}/api/sportingEvents`)
-    .set({
-      Authorization: `Bearer ${this.tempToken}`,
-    })
-    .end((err, res) => {
-      if (err) return done(err);
-      expect(res.status).toEqual(200);
-      expect(res.body.name).toEqual(exampleSportingEvent.name);
-      expect(res.body.desc).toEqual(exampleSportingEvent.desc);
-      expect(res.body.teamID).toEqual(this.tempTeam.id.toString());
-      done();
-    });
-});
-
-it('should return a 401 when no token is provided', done => {
-  request.get(`${url}/api/sportingEvents`)
-    .set({
-      Authorization: 'Bearer',
-    })
-    .end((err, res) => {
-      expect(res.status).toEqual(401);
-      done();
-    });
-});
+// Don't write any code below this line
