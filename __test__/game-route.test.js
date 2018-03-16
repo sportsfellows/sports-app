c607c5c90ffa7b488bba93139d86cf1a393a0420
Bessie Arino <brarino@gmail.com>
2018-03-15 19:32:15 -0700

    started game route testing

--- __test__/game-route.test.js
+++ __test__/game-route.test.js
@@ -1,21 +1,68 @@
-// 'use strict';
+'use strict';
 
-// const request = require('superagent');
-// const User = require('../model/user/user.js');
-// const serverToggle = require('../lib/server-toggle.js');
-// const server = require('../server.js');
+const request = require('superagent');
+const faker = require('faker');
+const fakeTeam = require('./lib/fakeTeam.js');
+const Game = require('../model/league/userPick.js');
+const UserPick = require('../model/league/userPick.js');
+const serverToggle = require('../lib/server-toggle.js');
+const server = require('../server.js');
 
-// require('jest');
+require('jest');
 
-// const url = 'http://localhost:3000';
+const url = 'http://localhost:3000';
 
-// const {exampleUser, exampleProfile, exampleTeam, exampleSportingEvent, exampleGame, exampleLeague, exampleGroup, exampleComment} = require('./lib/mockData.js'); // eslint-disable-line
+let exampleGame = {
+  groupName: faker.company.companyName(),
+  privacy: 'public',
+};
 
-// describe('Game routes', function() {
-//   beforeAll( done => {
-//     serverToggle.serverOn(server, done);
-//   });
-//   afterAll( done => {
-//     serverToggle.serverOff(server, done);
-//   });
-// });
+let exampleUserPick = {
+
+};
+
+describe('Game Routes', function () {
+  beforeAll(done => {
+    serverToggle.serverOn(server, done);
+  });
+  afterAll(done => {
+    serverToggle.serverOff(server, done);
+  });
+
+  beforeEach( () => {
+    return fakeTeam.create()
+      .then(mock => {
+        this.home.mock = mock;
+      });
+  });
+
+  beforeEach( () => {
+    return fakeTeam.create()
+      .then(mock => {
+        this.away.mock = mock;
+      });
+  });
+
+  beforeEach(done => {
+    exampleGroup.owner = this.mock.profile.userID;
+    exampleGroup.users = [this.mock.profile.userID];
+
+    new Group(exampleGroup).save()
+      .then(group => {
+        this.tempGroup = group;
+        done();
+      })
+      .catch(done);
+  });
+
+  afterEach(done => {
+    Promise.all([
+      fakeProfile.remove,
+      Group.remove({}),
+    ])
+      .then(() => done())
+      .catch(done);
+  });
+
+
+});
